3bfb7fbbef5085d69ae142d3fb0120c72a55b28f
cmitchell575 <53196998+cmitchell575@users.noreply.github.com>
2024-12-12 17:23:37 +0000

    I fixed it!

--- MS.cpp
+++ MS.cpp
@@ -2,83 +2,71 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-int main() {
-  const int boardSize = 3; 
-  const int numBombs = 2; 
-  char displayBoard[boardSize][boardSize]; 
-  bool isBomb[boardSize][boardSize] = {false}; 
-  int adjacentBombs[boardSize][boardSize] = {0};
 
-  for (int i = 0; i < boardSize; i++){
-    for (int j = 0; j < boardSize; j++) {
-      displayBoard[i][j] = 'X' ;
-    }
-  }
+int main() {
+  char board[3][3] = {{'a','b','c'},{'d','e','f'},{'g','h','i'}};
+  bool isBomb[3][3] = {{false,false,false},{false,false,false},{false,false,false}}; 
+  int adjacentBombs[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+  int bombsPlaced = 0;
+  int revealedSpaces= 0;
+  int totalSpaces = 9;
+  int totalBombs = 2;
+
   srand(static_cast<unsigned int>(time(0)));
-  for (int i = 0; i < numBombs;) {
-    int x = rand() % boardSize;
-    int y = rand() % boardSize;
+  while (bombsPlaced < totalBombs) {
+    int x = rand() % 3;
+    int y = rand() % 3;
     if (!isBomb[x][y]){
         isBomb[x][y] = true;
-        i++;
+        bombsPlaced++;
     }
   }
-  for (int i = 0; i < boardSize; i++) {
-    for (int j = 0; j < boardSize; j++) {
-      if (!isBomb[i][j]);
-        for (int dx = -1; dx <= 1; dx++) {
-          for (int dy = -1; dy <= 1; dy++) {
-            int nx = i + dx;
-            int ny = j + dy;
-            if (dx!= 0 || dy != 0){
-              if (nx >= 0 && nx < boardSize && ny >= 0 && ny < boardSize && isBomb[nx][ny]){
-                adjacentBombs[i][j]++;
-                 }
-              }
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      if (isBomb[x][y]) continue;
+      for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+          int nx = x + dx;
+          int ny = y + dy;
+            if (nx >= 0 && nx < 3 && ny >= 0 && ny < 3 && isBomb[nx][ny]){
+              adjacentBombs[x][y]++;
             }
+          }
         }
       }
     }
-  bool gameOver = false;
-  bool gameWon = false;
-  while (!gameOver && !gameWon) {
-    for (int i = 0; i < boardSize; i++) {
-      for (int j = 0; j < boardSize; j++) {
-        cout << "| " << displayBoard[i][j] << " ";
+  while (revealedSpaces < totalSpaces - totalBombs) {
+    cout << "\nCurrent Board:\n";
+    for (int i = 0; i < 3; i++) {
+      cout << "| " << board[i][0] << " | " << board [i][1] << " | " << board[i][2] << " |\n";
+    }
+    char input;
+    cout << "Enter a cell to reveal the sapce (a-i): ";
+    cin >> input;
+    bool matchInput = false;
+    for (int x = 0; x < 3; x++) {
+      for (int y = 0; y < 3; y++){
+        if (board[x][y] == input){
+          matchInput = true;
+          if (isBomb[x][y]) {
+            cout << "\nGame Over! You hit a bomb!\n";
+            for (int i = 0; i < 3; i++){
+              for (int j = 0; j < 3; j++){
+                if (isBomb[i][j]) board[i][j] = '*';
+                cout << "| " << board[i][j] << " ";
+              }
+              cout << "|\n";
+            }
+          }else {
+            board[x][y] = '0' + adjacentBombs[x][y];
+            revealedSpaces++;
+          }
+        }
       }
-      cout << "|\n";
     }
-  int x, y;
-  cout << "Enter row and column to reveal (0 = " << boardSize - 1<< "): ";
-  cin >> x >> y;
-  if (x < 0 || x >= boardSize || y < 0 || y >= boardSize) {
-    cout << "Invalid input. Try again.\n";
-  }else if (isBomb[x][y]){
-    gameOver = true;
-  }else{
-  displayBoard[x][y] = '0' + adjacentBombs[x][y];
-  gameWon = true;
-  for (int i = 0; i < boardSize; i++) {
-    for (int j = 0; j < boardSize; j++) {
-      if (displayBoard[i][j] == 'X' && !isBomb[i][j]){
-        gameWon = false;
-      }
+    if (!matchInput) {
+      cout << "Invalid input. Try again.\n";
     }
   }
-  }
-}
-for (int i = 0; i < boardSize; i++) {
-  for (int j = 0; j < boardSize; j++) {
-    if (isBomb[i][j]){
-      displayBoard[i][j] = '*';
-    }
-    cout << "| " << displayBoard[i][j] << " ";
-  }
-  cout << "|\n";
-}
-if (gameOver) {
-  cout <<"Game Over! You hit a bomb!\n";
-}else if(gameWon){
-  cout << "Congratulations! You've revealed all non-bomb spaces!\n";
-}
+  cout << "\nCongratulations! You've revealed all safe spaces!\n";
 }
